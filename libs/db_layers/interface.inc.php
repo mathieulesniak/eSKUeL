00751225d52abe1f30c7dda7f28ae69d395066e8
Mathieu LESNIAK <mathieu@lesniak.fr>
2012-03-18 11:16:09 +0100

    Interface definition for DB Layers

---
+++ libs/db_layers/interface.inc.php
@@ -0,0 +1,49 @@
+<?php
+interface db_layer {
+
+	// Reminder only
+	// Needed member vars
+	// var $specific_methods = array();
+	// var $field_types = array();
+	// var $field_functions = array();
+	
+	function connect();
+	
+	function query($sql);
+	
+	function get_results();
+
+	function num_rows();
+
+	// DB related functions
+
+	function db_list();
+
+	function db_get_tables_infos($db_name);
+
+	function db_create($db_name);
+
+	function db_delete($db_name);
+
+	// Table related functions
+
+	function table_get_fields($db_name, $table_name);
+
+	function table_get_indexes($db_name, $table_name);
+
+	function table_copy($db_from, $table_from, $db_to, $table_to, $copy_with_data);
+
+	function table_move();
+
+	function table_rename($db_old_name, $old_name, $db_new_name, $new_name);
+
+	function table_add_field();
+
+	function table_change_type();
+
+	function table_empty($db_name, $table_name);
+
+
+}
+
+?>
