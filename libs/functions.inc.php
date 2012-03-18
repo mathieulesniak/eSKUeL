a5370850deb8814229871b1425292df4bf7b5b23
loranger <loranger@free.fr>
2012-03-18 19:46:39 +0100

    [Upg] $_POST and $_GET generic handlers

--- libs/functions.inc.php
+++ libs/functions.inc.php
@@ -1,4 +1,54 @@
 <?php
+
+ /**
+  * Return a post parameter value
+  *
+  * @param string $name of the requested value
+  * @param mixed $default value
+  * @return mixed value of the post parameter, or default value, or false
+  **/
+ function getPostParam($name, $default = false)
+ {
+ 	if( array_key_exists($name, $_POST) )
+	{
+ 		return $_POST[$name];
+	}
+ 	return $default;
+ }
+
+
+/**
+ * Return a post or get parameter value
+ *
+ * @param string $name of the requested value
+ * @param mixed $default value
+ * @return mixed value of the post or get parameter, or default value, or false
+ **/
+function getParam($name, $default = false)
+{
+	if( $post = getPostParam($name) )
+	{
+		return $post;
+	}
+	else if( array_key_exists($name, $_GET) )
+	{
+		return $_GET[$name];
+	}
+	return $default;
+}
+
+/**
+ * Detect if a post or get parameter exists
+ *
+ * @param string $name of the requested value
+ * @return boolean
+ **/
+function hasParam($name)
+{
+	return ( getParam($name) !== false );
+}
+
+
 function _p($var) {
 	echo '<pre>';
 	print_r($var);
