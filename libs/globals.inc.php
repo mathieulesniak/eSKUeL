43f96b0b589d40e47b4d380c40aaf9a10e01361f
Mathieu LESNIAK <mathieu@lesniak.fr>
2012-03-18 11:29:01 +0100

    global file calling all libs

---
+++ libs/globals.inc.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+ini_set('html_errors', true);
+
+require_once 'object_simple_object.inc.php';
+require_once 'object_controller.inc.php';
+require_once 'functions.inc.php';
+require_once 'db_layers/interface.inc.php';
+require_once 'db_layers/mysql.inc.php';
+
+require_once 'object_database.inc.php';
+require_once 'object_table.inc.php';
+require_once 'object_field.inc.php';
+require_once 'object_index.inc.php';
+
+?>
