a9eef2421f0047130b031ad191b5cbfc810201bf
Mathieu LESNIAK <mathieu@lesniak.fr>
2012-03-18 20:56:59 +0100

    Convert from bytes to human readable

--- libs/functions.inc.php
+++ libs/functions.inc.php
@@ -5,4 +5,11 @@ function _p($var) {
 	echo '</pre>';
 }
 
+function convert_from_bytes($bytes) {
+   $unit = array(__('o'), __('Ko'),__('Mo'),__('Go'),__('To'),__('Po'),__('Eo'));
+   $precision = 1;
+   
+   return round($bytes / pow(1024, ($i = floor(log($bytes, 1024)))), $precision).' '.$unit[$i];
+}
+
 ?>
