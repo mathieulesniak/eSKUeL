7dd97cb115e4d731756b26a4197587100e7cfa69
Mathieu LESNIAK <mathieu@lesniak.fr>
2012-03-27 23:12:21 +0200

    [Upg] to camelCase & fix getTables()

--- libs/object/object_database.inc.php
+++ libs/object/object_database.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-class database extends simple_object
+class Database extends SimpleObject
 {
 
 	var $properties = array(
@@ -13,7 +13,7 @@ class database extends simple_object
 	var $private_properties = array(
 		'_sql_handler'
 	);
-
+    
 	function __construct($name, $sql_handler)
 	{
 		$this->name				= $name;
@@ -25,8 +25,7 @@ class database extends simple_object
 
 	static function load($name, $sql_handler)
 	{
-		$database = new database($name, $sql_handler);
-		//$database->get_tables();
+		$database = new Database($name, $sql_handler);
 
 		return $database;
 	}
@@ -37,50 +36,13 @@ class database extends simple_object
 
 	function delete()
 	{
-		$this->_sql_handler->db_delete($this->name);
+		return $this->_sql_handler->dbDelete($this->name);
 	}
 
-	function get_tables()
+	function getTables()
 	{
-		$tables_data = $this->_sql_handler->db_get_tables_infos($this->name)->get_results();
+        return $this->_sql_handler->dbGetTablesInfos($this->name);
 
-		if ( $tables_data !== false )
-		{
-			$tables 	= array();
-			$name_index = NULL;
-
-			foreach ( $tables_data['field'] as $index=>$field ) {
-				if ( $field == 'Name' ) {
-					$name_index = $index;
-					break;
-				}
-			}
-
-
-			if ( $name_index !== NULL ) {
-				foreach ( $tables_data['record'] as $record )
-				{
-						$resultset = array_combine($tables_data['field'], $record);
-						$current_table = table::load_from_array($resultset, $this->_sql_handler);
-						
-						$current_table->database = $this;
-
-						// Compute stats
-						$this->total_size	+= $current_table->data_length + $current_table->index_length;
-						$this->total_rows	+= $current_table->rows;
-
-						$tables[$name_index] = $current_table;
-					
-				}
-			}
-			$this->tables = $tables;
-		}
-		else
-		{
-			throw new ObjectException( ObjectException::GET_TABLES_FAIL, $this->name);
-		}
-
-		return $this;
 	}
 
 }
